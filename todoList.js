cdc764ae7cd87e016110dfdce6e7047f5a7f5b1c
Sunil Basnet <40550493+sbasnet7227@users.noreply.github.com>
2019-10-25 16:24:21 -0700

    Update todoList.js

--- todoList.js
+++ todoList.js
@@ -1,24 +1,40 @@
-var List = document.querySelectorAll('li');
+// JavaScript source code
+$('ul').on('click', 'li',function (event) {
+    $(this).toggleClass("complete");
+    //alert('hit li');
+    //event.stopPropagation();
 
-for( var i = 0 ; i < List.length; i++){
-    List[i].addEventListener('mouseover', function(){
-        this.classList.add('mouseOver');
-        
+});
 
+// click on X to delete ToDo
+$('ul').on('click', 'span',function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
     });
-
-    List[i].addEventListener('mouseout', function(){
-
-        this.classList.remove('mouseOver');
+    //$(this).parent().remove();
+    //alert('hit span');
+    event.stopPropagation(); // this event will stop firing up 
         
-    });
+});
 
-    List[i].addEventListener('click', function(){
-        this.classList.toggle('done');
-        
-    });
-  
-    
-    
+// dynamically creating new li by inputing text
 
-}
+$('input[type = "text"]').keypress(function (event) {
+    if (event.which === 13) {
+
+        // grabing new  todo text from input
+        var todoText = $(this).val();
+
+        $(this).val('');
+        ''
+        $('ul').append("<li><span><i class='fa fa-trash'></i></span>" + todoText + '</li>')
+    }
+
+});
+
+$('.fa-plus').click(function () {
+
+    $('input[type = "text"]').fadeToggle();
+});
+
+
